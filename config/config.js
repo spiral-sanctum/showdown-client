d801bb9f51ae050ea6ea6ae1aec303fb3d7f6b79
Matteo <boynoy66@gmail.com>
2023-07-18 17:40:03 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'Mnemiic': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (1e0bc77f)";
+Config.version = "0.11.2 (b20ebcb1)";
 
 Config.routes = {
 	root: 'pokemonshowdown.com',
